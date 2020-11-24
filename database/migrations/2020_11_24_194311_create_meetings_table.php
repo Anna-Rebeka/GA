7d55c6dd745ed8545f28cd272c20597e6033db96
Anna-Rebeka <rebeka.horecka@gmail.com>
2020-11-24 20:45:07 +0100

    assignments and meeting

---
+++ database/migrations/2020_11_24_194311_create_meetings_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeetingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('author_id');
+            $table->foreignId('group_id');
+            $table->text('description');
+            $table->dateTime('when');
+            $table->timestamps();
+
+            $table->foreign('author_id')
+                ->references('id')
+                ->on('users');
+            
+            $table->foreign('group_id')
+                ->references('id')
+                ->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meetings');
+    }
+}
